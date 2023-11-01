16ab3387519d9bfd847c8b545502de370908a5a5
unknown <mail@aleq.dev>
2023-11-01 23:29:23 +0300

    dispatch

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from dataclasses import dataclass
+
+import apscheduler.jobstores.base
 from flask import Flask, request, render_template
 import requests
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
+
 
 @dataclass
 class User:
@@ -8,9 +14,23 @@ class User:
     name: str
 
 
+user_auth = str
+event_id = int
+job = object
+
+
+@dataclass
+class Dispatch:
+    auths: list[user_auth]
+    job: job
+
+
 users: list[User] = []
+scheduler = BackgroundScheduler()
+dispatches: dict[event_id, Dispatch] = {}
 
 
+scheduler.start()
 app = Flask(__name__)
 
 
@@ -83,6 +103,66 @@ def getinfo():
     print(j)
 
     return j
+
+
+@app.route("/adddispatch", methods=["POST"])
+def adddispatch():
+    content = request.json
+
+    auth = content["auth"]
+    id = content["id"]
+    date_time = content["datetime"]
+
+    run_time = datetime.strptime(date_time, '%d.%m.%Y %H:%M:%S')
+
+    dispatch = dispatches.get(id, None)
+
+    if dispatch is None:
+        job = scheduler.add_job(dispatch_enroll, 'date', run_date=run_time, timezone='Europe/Moscow', args=[id])
+        dispatches[id] = Dispatch(auths=[auth], job=job)
+    else:
+        dispatch.auths.append(auth)
+
+    return "ok"
+
+
+@app.route("/removedispatch", methods=["POST"])
+def removedispatch():
+    content = request.json
+
+    auth = content["auth"]
+    id = content["id"]
+
+    dispatch = dispatches.get(id, None)
+
+    if dispatch:
+        if auth in dispatch.auths:
+            dispatch.auths.remove(auth)
+            if len(dispatch.auths) == 0:
+                try:
+                    dispatch.job.remove()
+                except apscheduler.jobstores.base.JobLookupError:
+                    pass
+
+                dispatches.pop(id)
+
+    return "ok"
+
+
+@app.route("/dispatches", methods=["GET"])
+def getdispatches():
+    return [
+        (k,v.auths) for k, v in dispatches.items()
+    ]
+
+
+def dispatch_enroll(event_id):
+    print("dispatch_enroll: ", datetime.now(), event_id)
+
+    for auth in dispatches[event_id].auths:
+        url = f"https://online.sochisirius.ru/schedule?mobile&task=enroll&id={event_id}"
+        headers = {'authorization': auth}
+        print(auth, " - ", requests.post(url, headers=headers).content)
 
 
 if __name__ == '__main__':
